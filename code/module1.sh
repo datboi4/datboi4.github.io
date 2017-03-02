349044aba5120bee9f5d9f54964a59b5b781c96a
datboi4 <sambraga87@gmail.com>
2017-03-02 08:46:34 -0500

    Update module1.sh

--- code/module1.sh
+++ code/module1.sh
@@ -1,2 +1,5 @@
-#sam
-echo 'here come dat boi'
+#!/bin/sh
+#sam braga
+(cd ~ || exit 1; x=10) #This is an example of a subshell
+echo $x #when this command is run nothing will be displayed due to the vaule of x being in a subshell and is not remembered
+pwd #when this command is run it will display the same directory as you started in
