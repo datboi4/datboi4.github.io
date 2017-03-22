bb595278be7c4162f501afcaf7002aab9a5e158e
datboi4 <sambraga87@gmail.com>
2017-03-22 08:52:26 -0400

    Update module3.sh

--- code/module3.sh
+++ code/module3.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-mkdir mod3sam
-cd mod3sam
-ls
-touch 123 1 2
-ls
-echo *
+mkdir mod3sam  #creates directory called mod3sam
+cd mod3sam     #gos to directory called mod3sam
+ls              #list files in directory
+touch 123 1 2   #makes files called 123, 1, 2
+ls              #lists files in directory
+echo *       #will put names of all files in directory on screen
